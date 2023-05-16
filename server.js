77a007505e249cecd73c421d013c5d9c4ac46c8a
Zach Berger <zachhberger@gmail.com>
2023-05-16 15:12:52 -0400

    established by two HTML routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+
 app.listen(PORT, () =>
   console.log(`Now listening at http://127.0.0.1:${PORT}`)
-);
+)
